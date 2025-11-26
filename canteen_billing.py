b82f22d32c35e9f17b0f065e39ffb4c1493f5c3b
bhanuprakashyangani <bhanuprakashyangani@gmail.com>
2025-11-26 12:28:51 +0530

    Update canteen_billing.py

---
+++ canteen_billing.py
@@ -0,0 +1,23 @@
+    ## developing a canteen menu code -- Bhanu Prakash.
+
+menu = {
+    "Snacks": [
+        {"name": "Cheese Maggi", "cost": 110, "type": "Veg"},
+        {"name": "Samosa", "cost": 50, "type": "Veg"},
+        {"name": "Chicken Sandwich", "cost": 180, "type": "Non-Veg"}
+    ],
+    "Hot Beverages": [
+        {"name": "Filter Coffee", "cost": 80, "volume": "200ML"},
+        {"name": "Cappuccino", "cost": 150, "volume": "250ML"},
+        {"name": "Masala Chai", "cost": 40, "volume": "180ML"}
+    ],
+    "Ice Creams and Drinks": [
+        {"name": "Mango Juice", "cost": 40, "volume": "200ML"},
+        {"name": "Orange Juice", "cost": 35, "volume": "200ML"},
+        {"name": "Butterscotch Ice Cream Tub", "cost": 60, "volume": "250ML"},
+        {"name": "Chocolate Ice Cream Tub", "cost": 55, "volume": "200ML"}
+    ]
+}
+print("Welcome to the Canteen Menu!")
+
+## next step is select item from the category.
